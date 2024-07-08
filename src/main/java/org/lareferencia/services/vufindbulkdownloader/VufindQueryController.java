84c4b3fb5a8ac1ca6ca283c8290b81a95ab3ce4b
Jesiel Viana <jesielviana@proton.me>
2024-07-08 12:38:11 -0300

    add endpoint para consultar dados de memoria

--- src/main/java/org/lareferencia/services/vufindbulkdownloader/VufindQueryController.java
+++ src/main/java/org/lareferencia/services/vufindbulkdownloader/VufindQueryController.java
@@ -28,6 +28,7 @@ import org.springframework.core.io.FileSystemResource;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -307,6 +308,15 @@ public class VufindQueryController {
 				.header(HttpHeaders.CONTENT_DISPOSITION, "attachment;filename=" + file.getName())
 				.contentType(MediaType.parseMediaType("application/zip")).contentLength(file.length())
 				.body(resource);
+	}
+
+	@GetMapping("memory-status")
+	public MemoryStats getMemoryStatistics() {
+		MemoryStats stats = new MemoryStats();
+		stats.setHeapSize(Runtime.getRuntime().totalMemory());
+		stats.setHeapMaxSize(Runtime.getRuntime().maxMemory());
+		stats.setHeapFreeSize(Runtime.getRuntime().freeMemory());
+		return stats;
 	}
 
 }
